177e11a7ea357568ee07b9df6dccd7bf5703b879
Glowka96 <glowackiseb@interia.pl>
2022-07-06 18:30:23 +0200

    Interface family

--- src/main/java/interfaceTask/family/FamilyMember.java
+++ src/main/java/interfaceTask/family/FamilyMember.java
@@ -1,7 +1,7 @@
 package interfaceTask.family;
 
 public interface FamilyMember {
-    public default void presentYourself(){
+    default void presentYourself(){
         System.out.println("I am just a simple family member");
     }
     boolean isAdult();
